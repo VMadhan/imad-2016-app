8d24a700d8616125550193ada23652f8dda20903
VMadhan <vmadhann@gmail.com>
2016-09-30 12:53:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,10 +3,11 @@ var element = document.getElementById('main-text');
 element.innerHTML = "Madhan";
 
 var img = document.getElementById('madi');
+var mginLeft = 0;
 function moveRight () {
     mginLeft = marginLeft + 10;
     img.style.marginLeft = mginLeft + 'px';
 }
 img.onclick = function () {
-    var interval = setInterval(moveRight, 50);
+    var interval = setInterval(moveRight, 100);
 }
